9badf11e3d2581d947aef0e17b42c9bec59bc18e
pin0S <pete.g.lynch@gmail.com>
2020-09-24 17:23:17 +1000

    rules function written

--- computer.rb
+++ computer.rb
@@ -5,7 +5,8 @@ class Computer < Player
 
     attr_reader :name
 
-    def initialize(name: nil)
+    def initialize
+        #randomly assign name for computer
         @name = ['Gary', 'Professor Oak', 'Brock', 'Misty', 
                 'Ash Ketchum', 'Lass', 'Fisherman', 'Hiker',
                 'Super Nerd', 'Bug Catcher', 'Gambler'].sample(1)
---
+++ rules.rb
@@ -0,0 +1,9 @@
+
+def rules
+    puts "The rules are as follows..."
+    puts "1. Pick your starter"
+    puts "2. Fire beats grass, grass beats water, water beats fire"
+    puts "3. You will play what best of you choose in until all games are played"
+end
+
+rules
